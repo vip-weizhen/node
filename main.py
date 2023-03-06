ac24e84fd1f8d8d2dd3d95e42b84ef34ae9ab4c4
weizhen <47912513+vip-weizhen@users.noreply.github.com>
2023-03-06 01:34:06 +0000

    更新

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
+import time
 import requests
 from bs4 import BeautifulSoup
 
 urls = [
-    'https://cdn.weizhen.xyz/https://raw.githubusercontent.com/freefq/free/master/v2', 
-    'https://cdn.weizhen.xyz/https://raw.githubusercontent.com/Jsnzkpg/Jsnzkpg/Jsnzkpg/Jsnzkpg', 
-    'https://cdn.weizhen.xyz/https://raw.githubusercontent.com/ripaojiedian/freenode/main/sub',
-    'https://sub.sharecentre.online/sub',
-    'https://cdn.weizhen.xyz/https://raw.githubusercontent.com/1218749540/work/main/jdnew',
-    'https://freefq.neocities.org/free.txt'
-    ]
+  'https://cdn.weizhen.xyz/https://raw.githubusercontent.com/freefq/free/master/v2',
+  'https://cdn.weizhen.xyz/https://raw.githubusercontent.com/Jsnzkpg/Jsnzkpg/Jsnzkpg/Jsnzkpg',
+  'https://cdn.weizhen.xyz/https://raw.githubusercontent.com/ripaojiedian/freenode/main/sub',
+  'https://sub.sharecentre.online/sub',
+  'https://cdn.weizhen.xyz/https://raw.githubusercontent.com/1218749540/work/main/jdnew',
+  'https://freefq.neocities.org/free.txt'
+]
 
 with open('node.txt', 'a', encoding='utf-8') as f:
-   
-    for url in urls:
-        response = requests.get(url)
-        html_content = response.text
-        soup = BeautifulSoup(html_content, 'html.parser')
-        text_content = soup.get_text()
-        f.write(text_content)
+
+  for url in urls:
+    response = requests.get(url)
+    html_content = response.text
+    soup = BeautifulSoup(html_content, 'html.parser')
+    text_content = soup.get_text()
+    f.write(text_content)
+
+while True:
+  time.sleep(600)
